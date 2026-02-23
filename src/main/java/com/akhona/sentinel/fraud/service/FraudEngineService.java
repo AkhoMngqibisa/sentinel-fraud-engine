08f186fae1de0acf29ceba9ab4f357d6ad91964e
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-23 07:46:00 +0200

    Feat - Add a service FraudEngineService for business logic

--- src/main/java/com/akhona/sentinel/fraud/service/FraudEngineService.java
+++ src/main/java/com/akhona/sentinel/fraud/service/FraudEngineService.java
@@ -1,8 +1,13 @@
 package com.akhona.sentinel.fraud.service;
 
-import com.akhona.sentinel.fraud.repository.FraudDecisionRepository;
-import com.akhona.sentinel.fraud.rule.RuleRegistry;
+import com.akhona.sentinel.fraud.model.*;
+import com.akhona.sentinel.fraud.repository.*;
+import com.akhona.sentinel.fraud.rule.*;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FraudEngineService {
@@ -13,5 +18,31 @@ public class FraudEngineService {
     public FraudEngineService(RuleRegistry ruleRegistry, FraudDecisionRepository decisionRepository) {
         this.ruleRegistry = ruleRegistry;
         this.decisionRepository = decisionRepository;
+    }
+
+    public FraudDecision assess(Transaction transaction) {
+        int totalScore = 0;
+        List<String> triggered = new ArrayList<>();
+
+        for (FraudRule rule : ruleRegistry.getRules()) {
+            // Get Transaction rule results
+            RuleResult result = rule.check(transaction);
+
+            if (result.isTriggered()) {
+                totalScore += result.getScore();
+                triggered.add(rule.getRuleName());
+            }
+        }
+
+        boolean flagged = totalScore >= 70;
+        FraudDecision fraudDecision = FraudDecision.builder()
+                .transactionId(transaction.getId())
+                .fraudScore(totalScore)
+                .flagged(flagged)
+                .triggeredRules(triggered)
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        return decisionRepository.save(fraudDecision);
     }
 }
