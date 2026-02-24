ffd2377621cd790226254026b2a54b386d4bd1a8
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-24 06:00:00 +0200

    Feat - Add a query that gets recent transactions

--- src/main/java/com/akhona/sentinel/fraud/repository/TransactionRepository.java
+++ src/main/java/com/akhona/sentinel/fraud/repository/TransactionRepository.java
@@ -2,8 +2,18 @@ package com.akhona.sentinel.fraud.repository;
 
 import com.akhona.sentinel.fraud.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+
+    @Query("""
+            SELECT t FROM Transaction t
+            WHERE t.userId = :userId
+            AND t.timestamp >= :date
+            """)
+    List<Transaction> findRecentTransactions(String userId, LocalDateTime date);
 }
