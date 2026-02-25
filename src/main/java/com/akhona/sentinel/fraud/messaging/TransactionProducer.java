301ecd3b5f34052b68e6cad1e76b3e89722eb493
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-25 13:01:00 +0200

    Feat - Implement functionality that creates a templatye on TransactionProducer

--- src/main/java/com/akhona/sentinel/fraud/messaging/TransactionProducer.java
+++ src/main/java/com/akhona/sentinel/fraud/messaging/TransactionProducer.java
@@ -1,4 +1,19 @@
 package com.akhona.sentinel.fraud.messaging;
 
+import com.akhona.sentinel.fraud.model.Transaction;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransactionProducer {
+
+    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+
+    public TransactionProducer(KafkaTemplate<String, Transaction> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(Transaction transaction) {
+        kafkaTemplate.send("transaction-events", transaction);
+    }
 }
