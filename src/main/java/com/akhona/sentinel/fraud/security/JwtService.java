419ab9b55d31c212ea3353ca72da607740e2df65
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-25 05:27:00 +0200

    Feat - Add a method that extract the username

--- src/main/java/com/akhona/sentinel/fraud/security/JwtService.java
+++ src/main/java/com/akhona/sentinel/fraud/security/JwtService.java
@@ -3,16 +3,30 @@ package com.akhona.sentinel.fraud.security;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 public class JwtService {
 
+    private static final String KEY = "mySuperSecretKeyThatIsAtLeast32CharactersLong!";
+
     public String generateToken(String username) {
-        String KEY = "secret-key";
         return Jwts.builder()
                 .subject(username)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + 3600000)
-                ).signWith(Keys.hmacShaKeyFor(KEY.getBytes())).compact();
+                .expiration(new Date(System.currentTimeMillis() + 3600000))
+                .signWith(Keys.hmacShaKeyFor(KEY.getBytes()))
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        SecretKey secretKey = Keys.hmacShaKeyFor(KEY.getBytes());
+
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
     }
 }
