6243c8261accd449ed65f96f7b7ab858eb1e87f4
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-25 10:47:00 +0200

    Feat - Implement functionality that listen on TransactionConsumer

--- src/main/java/com/akhona/sentinel/fraud/messaging/TransactionConsumer.java
+++ src/main/java/com/akhona/sentinel/fraud/messaging/TransactionConsumer.java
@@ -1,4 +1,21 @@
 package com.akhona.sentinel.fraud.messaging;
 
+import com.akhona.sentinel.fraud.model.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
 public class TransactionConsumer {
+
+    @KafkaListener(topics = "transaction-events", groupId = "fraud-group")
+    public void listen(Transaction transaction) {
+
+        System.out.println("Received transaction: " + transaction.getId());
+
+        if (transaction.getAmount().compareTo(BigDecimal.valueOf(1000)) > 0) {
+            System.out.println("Potential Fraud Detected!");
+        }
+    }
 }
