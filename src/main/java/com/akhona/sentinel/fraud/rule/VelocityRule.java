e3925965dd5d8b731b33b5f47f025b3f9931efed
AkhoMngqibisa <akhomngqibisa@gmail.com>
2026-02-26 05:49:00 +0200

    Feat - Upgrade VelocityRule Using Redis Counter Instead of querying DB

--- src/main/java/com/akhona/sentinel/fraud/rule/VelocityRule.java
+++ src/main/java/com/akhona/sentinel/fraud/rule/VelocityRule.java
@@ -2,31 +2,46 @@ package com.akhona.sentinel.fraud.rule;
 
 import com.akhona.sentinel.fraud.model.Transaction;
 import com.akhona.sentinel.fraud.repository.TransactionRepository;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import java.time.Duration;
 
 @Component
 public class VelocityRule implements FraudRule {
 
     private final TransactionRepository transactionRepository;
+    private final StringRedisTemplate stringRedisTemplate;
 
-    public VelocityRule(TransactionRepository transactionRepository) {
+    public VelocityRule(TransactionRepository transactionRepository, StringRedisTemplate stringRedisTemplate) {
         this.transactionRepository = transactionRepository;
+        this.stringRedisTemplate = stringRedisTemplate;
     }
 
+//    @Override
+//    /*
+//     * Detect rapid transactions from the same user within a short window.
+//     * If a customer performs ≥ 3 transactions within 1 minute, flag fraud.
+//     */
+//    public RuleResult check(Transaction transaction) {
+//        LocalDateTime now = LocalDateTime.now().minusMinutes(1);
+//        List<Transaction> recentTransaction = transactionRepository.findRecentTransactions(transaction.getUserId(), now);
+//
+//        if (recentTransaction.size() >= 3) {
+//            return new RuleResult(true, 40);
+//        }
+//        return new RuleResult(false, 0);
+//    }
 
     @Override
-    /*
-     * Detect rapid transactions from the same user within a short window.
-     * If a customer performs ≥ 3 transactions within 1 minute, flag fraud.
-     */
     public RuleResult check(Transaction transaction) {
-        LocalDateTime now = LocalDateTime.now().minusMinutes(1);
-        List<Transaction> recentTransaction = transactionRepository.findRecentTransactions(transaction.getUserId(), now);
+        String key = "velocity: " + transaction.getUserId();
 
-        if (recentTransaction.size() >= 3) {
+        Long count = stringRedisTemplate.opsForValue().increment(key);
+
+        stringRedisTemplate.expire(key, Duration.ofMinutes(1));
+
+        if (count >= 3) {
             return new RuleResult(true, 40);
         }
         return new RuleResult(false, 0);
